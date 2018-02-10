bd4d01db818e1fc2bcbd29b12f114b3bad36f830
cucsi1 <36246902+cucsi1@users.noreply.github.com>
2018-02-09 22:18:39 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,13 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+var sueldo;
+var aumento;
+var nuevoSueldo;
 
-	
+sueldo= parseInt(document.getElementById("sueldo").value);
+aumento= sueldo *0.10;
+nuevoSueldo = sueldo + aumento;
+document.getElementById("resultado").value = nuevoSueldo.toFixed(2);
+
 }
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,9 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+edad=document.getElementById("edad").value;
 
+if( edad >=21 && edad <=15)
 
 }//FIN DE LA FUNCIÓN
