9d5001250a71cdc12ccb788a6c5f194b0be1aec1
cucsi1 <36246902+cucsi1@users.noreply.github.com>
2018-02-16 22:12:07 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
-	alert('iteración while');
+	
+	var numero =1;
+	
+	while(numero <=10){
+		console.log (numero);
+		numero++;
+	}
+	
+
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,11 @@
 function Mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+var clave;
+clave = prompt("ingrese el número clave.");
+while(clave != "utn750")
+clave=prompt("error ingrese clave");
 
 
+alert("clave correcta");
 }//FIN DE LA FUNCIÓN
